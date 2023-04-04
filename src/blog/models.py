a7b67bf1db674e0432f7d47285599e2ef51fe5b8
krlslman <selmankorall@gmail.com>
2023-04-04 16:01:58 +0300

    Added MEDIA_ROOT

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta: # To fix the class name in admin dashboard when its are plural
+        verbose_name_plural = "Categories"
+
+    def __str__(self): # To prevent it's content appearing as "Category object" in admin dashboard
+        return self.name 
+
+
+class Post(models.Model):
+    OPTIONS = (
+        ('d', 'Draft'), # (database_log_name, user_shown_name)
+        ('p', 'Published'),
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True, unique=True)
+    def __str__(self):
+        return self.title
