b11fa9495d37cf5ce4e0d933bd1af40180494c2c
krlslman <selmankorall@gmail.com>
2023-04-05 05:21:10 +0300

    added post_list function

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):  # to list our post on the page
+    qs = Post.objects.all()  
+    context = {
+        "object_list": qs
+    }
+    return render(request, "blog/post_list.html", context)
--- src/blog_app/urls.py
+++ src/blog_app/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include("blog.urls"))
+    path("", include("blog.urls"))
 ]
 
 if settings.DEBUG:   # If debug=true in settings (before going production), pull images from the path I specified
