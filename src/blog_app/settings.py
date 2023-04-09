c3782eb89e61b7fa54412c1a8d8ff7391efca24b
krlslman <selmankorall@gmail.com>
2023-04-09 11:07:30 +0300

    better form view with crispy

--- src/blog_app/settings.py
+++ src/blog_app/settings.py
@@ -22,7 +22,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    # my apps:
+    'blog.apps.BlogConfig',
+    # third party:
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -110,3 +114,6 @@ MEDIA_ROOT = BASE_DIR / "media_root"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
