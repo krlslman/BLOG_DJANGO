b09fc72ba0201bafca1d0b3f5973f5656186470a
krlslman <selmankorall@gmail.com>
2023-04-10 03:17:20 +0300

    added logout page  |  fixed login_redirect_url

--- src/users/urls.py
+++ src/users/urls.py
@@ -4,9 +4,10 @@ from .views import register, profile
 
 
 urlpatterns = [    
-    # path("logout/", auth_views.LoginView.as_view(), name='logout'),
     path("register/", register, name='register'),
     path("profile/", profile, name='profile'),
     path("login/", auth_views.LoginView.as_view(
         template_name="users/login.html"), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(
+        template_name="users/logout.html"), name='logout'),
 ]
