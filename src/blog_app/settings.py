003c364d90c5a20e38695ab4b0f87228876dc64b
krlslman <selmankorall@gmail.com>
2023-04-10 16:37:17 +0300

    added password reset

--- src/blog_app/settings.py
+++ src/blog_app/settings.py
@@ -122,3 +122,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_REDIRECT_URL = "blog:list"
 
 LOGIN_URL = "login"  # prevent error and redirect login when url /create
+
+#Sending email (allow IMAP from gmail settings!)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
