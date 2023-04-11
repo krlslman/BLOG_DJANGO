e39b44546dc35ed4866f3be6e3b59476124e7db3
krlslman <selmankorall@gmail.com>
2023-04-11 06:55:21 +0300

    set railway deployment settings

--- src/blog_app/settings.py
+++ src/blog_app/settings.py
@@ -67,9 +67,17 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'railway',
+        'USER': 'postgres',
+        'PASSWORD': config("RAILWAY_DB_PASSWORD"),
+        'HOST': config("RAILWAY_DB_HOST"),
+        'PORT': '6076',
     }
 }
 
@@ -112,9 +120,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR / "static"),
+# ]
 
 MEDIA_URL = 'media/'
 
